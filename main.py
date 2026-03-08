15158c9811c2bd2af6195de4c07be4f95a794f6c
HiMindawang <minfu3131@gmail.com>
2026-03-08 21:50:02 +0000

    Add dp

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from pygumby!")
+import getpass
+import os
 
+import dotenv
+import dead_parrot as dp
 
-if __name__ == "__main__":
-    main()
+dotenv.load_dotenv()
+
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+ecb_ai_assistant: dp.AiAssistant = dp.DspyAiAssistant(
+    lm="openai/gpt-4o-mini",
+    embedder="openai/text-embedding-3-small",
+    corpus=dp.utils.load_corpus_from_pdf(
+        "DMD",
+        "context/dmd.pdf",
+    ),
+    examples=dp.utils.load_examples_from_json("examples/x.json"),
+)
+print(ecb_ai_assistant.ask("what is data processing cycle? And how many stages are there in it?"))
+
